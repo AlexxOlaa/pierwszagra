3a0888aed6dbed3c9b248d49ffb5e7b2927a5b0f
Ola <ola.bara@onet.pl>
2022-06-21 00:46:03 +0200

    fajnie

--- game.cpp
+++ game.cpp
@@ -3,63 +3,66 @@
 using namespace sf;
 
 int main()
-{   /*
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }*/
+{   
     srand(time(0));
-
     RenderWindow window(VideoMode(400, 400), "Saper!");
 
-    int w = 32;
-    int grid[12][12];
-    int sgrid[12][12]; //for showing
+    Texture texture;
+    texture.loadFromFile("sztuka.jpg");
+    Sprite sprite(texture);
 
-    Texture t;
-    t.loadFromFile("tiles.jpg");
-    Sprite s(t);
+    int area[12][12];
+    int showarea[12][12]; 
 
     for (int i = 1; i <= 10; i++)
+    {
         for (int j = 1; j <= 10; j++)
         {
-            sgrid[i][j] = 10;
-            if (rand() % 5 == 0)  grid[i][j] = 9;
-            else grid[i][j] = 0;
+            showarea[i][j] = 10;
+            if (rand() % 5 == 0)
+            {
+                area[i][j] = 9;
+            }
+            else
+            {
+                area[i][j] = 0;
+            }
         }
+    }
 
     for (int i = 1; i <= 10; i++)
+    {
         for (int j = 1; j <= 10; j++)
         {
             int n = 0;
-            if (grid[i][j] == 9) continue;
-            if (grid[i + 1][j] == 9) n++;
-            if (grid[i][j + 1] == 9) n++;
-            if (grid[i - 1][j] == 9) n++;
-            if (grid[i][j - 1] == 9) n++;
-            if (grid[i + 1][j + 1] == 9) n++;
-            if (grid[i - 1][j - 1] == 9) n++;
-            if (grid[i - 1][j + 1] == 9) n++;
-            if (grid[i + 1][j - 1] == 9) n++;
-            grid[i][j] = n;
+            if (area[i][j] == 9)
+                continue;
+            if (area[i + 1][j] == 9)
+                n++;
+            if (area[i][j + 1] == 9)
+                n++;
+            if (area[i - 1][j] == 9)
+                n++;
+            if (area[i][j - 1] == 9)
+                n++;
+            if (area[i + 1][j + 1] == 9)
+                n++;
+            if (area[i - 1][j - 1] == 9)
+                n++;
+            if (area[i - 1][j + 1] == 9)
+                n++;
+            if (area[i + 1][j - 1] == 9)
+                n++;
+           area[i][j] = n;
         }
+    }
 
     while (window.isOpen())
     {
+        int width = 32;
         Vector2i pos = Mouse::getPosition(window);
-        int x = pos.x / w;
-        int y = pos.y / w;
+        int x = pos.x / width;
+        int y = pos.y / width;
 
         Event event;
         while (window.pollEvent(event))
@@ -68,8 +71,14 @@ int main()
                 window.close();
 
             if (event.type == Event::MouseButtonPressed)
-                if (event.key.code == Mouse::Left) sgrid[x][y] = grid[x][y];
-                else if (event.key.code == Mouse::Right) sgrid[x][y] = 11;
+                if (event.key.code == Mouse::Left)
+                {
+                    showarea[x][y] = area[x][y];
+                }
+                else if (event.key.code == Mouse::Right)
+                {
+                    showarea[x][y] = 11;
+                }
         }
 
         window.clear(Color::Blue);
@@ -77,10 +86,13 @@ int main()
         for (int i = 1; i <= 10; i++)
             for (int j = 1; j <= 10; j++)
             {
-                if (sgrid[x][y] == 9) sgrid[i][j] = grid[i][j];
-                s.setTextureRect(IntRect(sgrid[i][j] * w, 0, w, w));
-                s.setPosition(i * w, j * w);
-                window.draw(s);
+                if (showarea[x][y] == 9)
+                {
+                    showarea[i][j] = area[i][j];
+                }
+                sprite.setTextureRect(IntRect(showarea[i][j] * width, 0, width, width));
+                sprite.setPosition(i * width, j * width);
+                window.draw(sprite);
             }
 
         window.display();
